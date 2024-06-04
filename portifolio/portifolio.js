3cf0819a0825d0e23f37c3a318807d3dd19d6731
Rafael Felipe <rafael12090812@gmail.com>
2024-06-04 14:05:29 -0300

    Add files via upload

---
+++ portifolio/portifolio.js
@@ -0,0 +1,24 @@
+
+function Mostrasite1(){
+    let site = document.querySelector('#site1');
+    site.style.display = 'block';
+    let site2 = document.querySelector('#site2');
+    site2.style.display = 'none';
+}
+
+function Fechasite1(){
+let site = document.querySelector('#site1');
+site.style.display = 'none';
+}
+
+function Mostrasite2(){
+    let site = document.querySelector('#site2');
+    site.style.display = 'block';
+    let site2 = document.querySelector('#site1');
+    site2.style.display = 'none';
+}
+
+function Fechasite2(){
+let site = document.querySelector('#site2');
+site.style.display = 'none';
+}
